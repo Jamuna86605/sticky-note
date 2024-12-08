358c3ffaae52363332d674d516449e2b8ed01998
Jamuna86605 <jamunaks911@gmail.com>
2024-12-08 13:53:55 +0530

    project complete

---
+++ app.js
@@ -0,0 +1,28 @@
+//function createNote(){
+    // const stickyNote = document.querySelector(".sticky-notes");
+    // const noteContainer = document.createElement("div");
+    // noteContainer.classList.add("note-container");
+    // const noteContent = document.createElement("div");
+    // noteContent.classList.add("note-content");
+    // noteContent.contentEditable=true;
+    // noteContent.textContent="new note";
+
+    // const noteActions = document.createElement("div");
+    // noteActions.classList.add("note-actions");
+
+    // const deleteButton = document.createElement("button");
+    // deleteButton.classList.add("delete-note");
+    // deleteButton.textContent = "Delete";
+    // deleteButton.onclick = function(){
+    //     noteContainer.remove();
+    // };
+    // noteActions.appendChild(deleteButton);
+    // noteContainer.appendChild(noteContent);
+    // noteContainer.appendChild(noteActions);
+    // stickyNote.appendChild(noteContainer);
+
+//}
+function createNote(){
+    alert("hello");
+}
+
